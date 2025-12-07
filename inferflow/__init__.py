31b5dcf22e05ca382d5aad85384f1c6cb9ea494f
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-12-08 00:06:51 +0800

    feat(init): add support for optional C++ extensions in the package

--- inferflow/__init__.py
+++ inferflow/__init__.py
@@ -1,3 +1,12 @@
 from __future__ import annotations
 
+try:
+    from inferflow import _C
+
+    HAS_CPP_EXTENSIONS = True
+except ImportError:
+    _C = None
+    HAS_CPP_EXTENSIONS = False
+
+__all__ = ["_C", "HAS_CPP_EXTENSIONS"]
 __version__ = "0.1.0a2"
